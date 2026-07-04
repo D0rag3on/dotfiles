f4098df3691ab66a420b8d87f173efb9aad2d5a1
D0rag3on <noel.neuhoff@itc-studenten.de>
2026-07-04 16:05:18 +0200

    cleanup

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,8 @@
 if status is-interactive
 # Commands to run in interactive sessions can go here
   alias con="sudo -e /home/nono/dotfiles/nixos/configuration.nix"
-  alias reb="sudo -E nixos-rebuild switch"
+  alias reb="sudo nixos-rebuild switch --flake /home/nono/dotfiles/nixos/.config/nixos"
+  alias upg="sudo nixos-rebuild switch --upgrade --flake /home/nono/dotfiles/nixos/.config/nixos"
   alias sudo="sudo "
 end
 
-set -x NIX_PATH $NIX_PATH nixos-config=/home/nono/dotfiles/nixos/.config/nixos/configuration.nix
